c3c3eeae75da889b1236103ca7d61de5e21dc945
jesussiancasf <jesussianflo@gmail.com>
2022-05-29 15:40:14 -0500

    Este es un proyecto mini del juego ahorcado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project';
+   palabra='AGUACATE';
+   palabraOculta='';
+   intentos=0;
+   gano=false;
+   perdio=false;
+
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  
+  constructor() {
+
+    this.palabraOculta='_ '.repeat(this.palabra.length);
+
+  }
+  comprobar(letra: string){
+
+    this.letras.splice(this.letras.indexOf(letra),1);
+    this.existeLetra(letra);
+    const palabraOcultaArr=this.palabraOculta.split(' ');
+    
+    for (let i = 0; i < this.palabraOculta.length; i++) {
+      
+      if(this.palabra[i]===letra){
+        
+        palabraOcultaArr[i]=letra;
+      }
+    }
+    this.palabraOculta=palabraOcultaArr.join(' ');
+    this.verificaVictoria();
+  }
+
+  existeLetra(letra:string){
+    if(this.palabra.indexOf(letra)>=0){
+    }
+    else{
+      this.intentos++;
+    }
+  }
+
+  verificaVictoria(){
+    const palabraArr=this.palabraOculta.split(' ');
+    const palabraEvaluar=palabraArr.join('');
+
+    if(palabraEvaluar===this.palabra){
+      this.gano=true;
+      console.log('user wins')
+    }
+
+    if(this.intentos>=9){
+      this.perdio=true;
+      console.log('user perdio')
+
+    }
+  }
+
 }
